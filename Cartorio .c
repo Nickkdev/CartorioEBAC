f5ada0337895dc1d672636fec148ad13fb9379d7
Nickkdev <nicknaespera@gmail.com>
2024-05-03 17:43:33 -0300

    Atualizaçao de uma nova linha
    Adicionei a opção de sair para o usuário

--- Cartorio .c
+++ Cartorio .c
@@ -131,7 +131,7 @@ int main(){
     	printf("\t1 - Registrar nomes\n");
     	printf("\t2 - Consultar nomes\n");
     	printf("\t3 - Deletar nomes\n\n"); //fim do menu
-	
+	    printf("\t4 - Sair do sistema\n\n");            
 	
     	scanf("%d", &opcao); //armazenado a escolha do usuario
 	
@@ -151,7 +151,11 @@ int main(){
     			deletar();
     			break;
     			
-    			
+    		case 4:
+    			printf("Obrigado por utilizar o sistema!");
+    			return 0;
+    			break;
+			    	
     			
     			
     				default:
